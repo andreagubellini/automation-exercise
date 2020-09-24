dd88d6935df8dd7f050cfe175eca3c00c1b435a2
Andrea Gubellini <andreagubellini@gmail.com>
2020-09-24 23:37:15 +0200

    adds docker sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+VERSION="latest"
+TESTDIR=atests
+SUBTESTDIR=
+HOST_TESTDIR=.
+ARGS="--console-verbose"
+THREADS="0"
+docker run --rm \
+           -v $PWD/$HOST_TESTDIR:/$TESTDIR/ \
+           -v $PWD/output:/outputs \
+           -e ROBOTARGS=$ARGS \
+           -e THREADS=$THREADS \
+           -e TESTDIR=$TESTDIR \
+           -e SUBTESTDIR=$SUBTESTDIR \
+           robotframework-docker:$VERSION
